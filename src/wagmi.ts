b7fea777e2da5fdaefc6beb14e41ca6bd5cf7f9a
Saeid Hosseinabadi <madman9494@protonmail.com>
2024-11-24 21:13:56 +0330

    fix network

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,14 +1,14 @@
 import { http, createConfig } from 'wagmi'
-import { lineaSepolia } from 'wagmi/chains'
+import { linea } from 'wagmi/chains'
 import { injected } from 'wagmi/connectors'
 
 export const config = createConfig({
-  chains: [lineaSepolia],
+  chains: [linea],
   connectors: [
     injected()
   ],
   transports: {
-    [lineaSepolia.id]: http()
+    [linea.id]: http()
   },
 })
 

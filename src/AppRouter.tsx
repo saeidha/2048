002cb0d769603662fbe5969e829c657f5dee30a0
saeed221373 <saeed221373@googlemail.com>
2025-12-04 20:04:09 +0000

    fix

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -6,41 +6,32 @@ import SupportPage from './support';
 import PrivacyPolicy from './PrivacyPolicy';
 
 const AppRouter = () => {
-  const host = window.location.host;
+  const mainPage = import.meta.env.VITE_MAIN_PAGE || 'LANDING';
 
-  if (host === 'app.2028game.site') {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<GamePage />} />
-        </Routes>
-      </Router>
-    );
-  } else if (host === 'support.2028game.site') {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<SupportPage />} />
-        </Routes>
-      </Router>
-    );
-  } else if (host === 'privacy-policy.2028game.site') {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<PrivacyPolicy />} />
-        </Routes>
-      </Router>
-    );
-  } else {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-      </Router>
-    );
+  let element: React.ReactElement;
+
+  switch (mainPage) {
+    case 'APP':
+      element = <GamePage />;
+      break;
+    case 'SUPPORT':
+      element = <SupportPage />;
+      break;
+    case 'PRIVACY':
+      element = <PrivacyPolicy />;
+      break;
+    default:
+      element = <LandingPage />;
+      break;
   }
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={element} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default AppRouter;
